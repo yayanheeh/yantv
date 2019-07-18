dfa2b261fa8def0cc2e2526136cbf5e81676bc11
yayanheeh <tokoxp58@gmail.com>
2019-07-18 11:47:27 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -6,9 +6,6 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-import { MDBContainer} from 'mdbreact'
-
-
 // Externals
 import PropTypes from 'prop-types';
 
