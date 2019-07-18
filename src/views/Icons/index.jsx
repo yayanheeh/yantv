972d84d9b84adf13d60e09ff30363c07a3120b82
yayanheeh <tokoxp58@gmail.com>
2019-07-18 13:24:07 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -38,27 +38,27 @@ class Icons extends Component {
      
 <div className={classes.root}>
   
-    <div 
-      className = "video" 
-      style = {{ 
-        position: "relative", 
-        paddingBottom: "56.25%" / * 16: 9 * /, 
-        paddingTop: 25, 
-        tinggi: 0 
-      }} 
-    > 
-      <iframe 
-        style = {{ 
-          position: "absolute", 
-          atas: 0, 
-          kiri: 0, 
-          lebar: "100%", 
-          tinggi: "100%" 
-        }} 
-        src = {` https://www.youtube. com / embed / $ {youtubeId} ` } 
-        frameBorder =" 0 " 
-      /> 
-    </div> 
+    <div
+      className="video"
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+        height: 0
+      }}
+    >
+      <iframe
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
+        }}
+        src={`https://www.youtube.com/embed/${youtubeId}`}
+        frameBorder="0"
+      />
+    </div>
 
 </div>    
 <div className={classes.root}>
