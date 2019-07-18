8ce9f9be1918d75df53bf58b9bc259f880c5f768
yayanheeh <tokoxp58@gmail.com>
2019-07-18 13:06:21 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -38,9 +38,7 @@ class Icons extends Component {
      
 <div className={classes.root}>
      
- <div className="yayan">.
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/C8kWkS5l73M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div></div>
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe width='560' height='315' src='https://www.youtube.com/embed/C8kWkS5l73M' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></div></div>
 
 <div className={classes.root}>
       
