7a3212823456c1c0c84ae8e7932b8499af6c374f
yayanheeh <tokoxp58@gmail.com>
2019-07-18 09:58:24 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 import CardMedia from '@material-ui/core/CardMedia';
 
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 
 // Externals
@@ -45,6 +47,32 @@ class Icons extends Component {
 
      </Paper>
 </div>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
+    
+
         </div>
       </DashboardLayout>
     );
