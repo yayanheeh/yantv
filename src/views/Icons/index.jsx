9f3606592c43b748218eed023f4886e669ad5cf7
yayanheeh <tokoxp58@gmail.com>
2019-07-18 11:58:15 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -37,11 +37,11 @@ class Icons extends Component {
       <DashboardLayout title="Icons">
         <div className={classes.root}>
         <yayan
-            className={classes.yayan}
+            className={classes.yayan}>
             <video width="250" controls>
   <source src="http://202.80.222.130/000001/2/ch00000090990000001730/index.m3u8?virtualDomain=000001.live_hls.zte.com" type="application/x-mpegURL">  
   </source>
-          />
+          </yayan>
 
 
               <div>
