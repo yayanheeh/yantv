4f1f5adb6fb5ae6f613aab6193f5af689dbb2355
yayanheeh <tokoxp58@gmail.com>
2019-07-21 00:13:43 +0700

    Update index.jsx

--- src/views/Test/index.jsx
+++ src/views/Test/index.jsx
@@ -52,12 +52,12 @@ class Icons extends Component {
       <DashboardLayout title="Yplay tv">
 		   
 		<Hidden smUp>
-         <video id="player" class="video-js vjs-default-skin" controls preload="auto" autoplay controls height="200" width="300">
-							<source src="http://202.80.222.130/000001/2/ch15010918464887721048/index.m3u8?virtualDomain=000001.live_hls.zte.com" type="application/x-mpegURL">
-		 </source></video>
+         
             
-            mobile
- <div style={videoContainer}>tes</div>
+            
+ <div style={videoContainer}><video id="player" class="video-js vjs-default-skin" controls preload="auto" autoplay controls height="200" width="300">
+							<source src="http://202.80.222.130/000001/2/ch15010918464887721048/index.m3u8?virtualDomain=000001.live_hls.zte.com" type="application/x-mpegURL">
+		 </source></video></div>
 
  </Hidden>
 		   
