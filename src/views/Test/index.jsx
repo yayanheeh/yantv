55062a95679a0275ee4917fde23efa48eb74aef7
yayanheeh <tokoxp58@gmail.com>
2019-07-20 23:29:41 +0700

    Update index.jsx

--- src/views/Test/index.jsx
+++ src/views/Test/index.jsx
@@ -19,8 +19,18 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import TabContainer from '@material-ui/core/TabContainer';
 
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 // Externals
 import PropTypes from 'prop-types';
