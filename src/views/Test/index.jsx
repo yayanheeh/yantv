63d8dd8cfb879e25825dfa8d92c1608c72d5987f
yayanheeh <tokoxp58@gmail.com>
2019-07-20 21:31:36 +0700

    Update index.jsx

--- src/views/Test/index.jsx
+++ src/views/Test/index.jsx
@@ -47,7 +47,7 @@ class Icons extends Component {
 		    
 	<div className={classes.root}>	
 		<Hidden smUp>
-          <video id="player" class="video-js vjs-default-skin" controls preload="auto" autoplay controls height="360" width="640">
+          <video id="player" class="video-js vjs-default-skin" controls preload="auto" autoplay controls height="250" width="300">
 							<source src="http://202.80.222.130/000001/2/ch15010918464887721048/index.m3u8?virtualDomain=000001.live_hls.zte.com" type="application/x-mpegURL">
             </source></video>
             
