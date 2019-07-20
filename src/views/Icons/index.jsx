2fb62581a0b7067db441fa462f691ea7791ec180
yayanheeh <tokoxp58@gmail.com>
2019-07-20 17:10:29 +0700

    Update index.jsx

--- src/views/Icons/index.jsx
+++ src/views/Icons/index.jsx
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
 
-import CardMedia from '@material-ui/core/CardMedia';
-
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-
 // Externals
 import PropTypes from 'prop-types';
 
@@ -14,15 +8,16 @@ import { withStyles } from '@material-ui/core';
 
 // Shared layouts
 import { Dashboard as DashboardLayout } from 'layouts';
-// Component styles video 
-
 
 // Component styles
 const styles = theme => ({
   root: {
     padding: theme.spacing.unit * 4
   },
-    embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%;
+  iframe: {
+    width: '100%',
+    minHeight: '640px',
+    border: 0
   }
 });
 
@@ -32,51 +27,12 @@ class Icons extends Component {
 
     return (
       <DashboardLayout title="Icons">
-     
-   
-<div className={classes.root}>
-      
-              <div>
-      <Paper className={classes.root}>
- 
-        <CardMedia>
-    <video width="200" controls>
-  <source src="http://202.80.222.130/000001/2/ch00000090990000001730/index.m3u8?virtualDomain=000001.live_hls.zte.com" type="application/x-mpegURL">  
-  </source>
-</video>
-    </CardMedia>
-
-     </Paper>
-</div>
-
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}><video width="250" controls>
-  <source src="http://202.80.222.130/000001/2/ch00000090990000001730/index.m3u8?virtualDomain=000001.live_hls.zte.com" type="application/x-mpegURL">  
-  </source>
-</video> </Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}> test</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>xs=6 sm=3</Paper>
-        </Grid>
-      </Grid>
-    
-
+        <div className={classes.root}>
+          <iframe
+            className={classes.iframe}
+            src="https://material.io/tools/icons/?icon=accessibility&style=outline"
+            title="Material Design icons"
+          />
         </div>
       </DashboardLayout>
     );
